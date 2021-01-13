8b5a9d861d846ac5d981533d2fb82c69f44c0c41
Septs <github@septs.pw>
2021-01-14 06:26:29 +0800

    add query coin info

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
-import { StorageRegistry } from './types';
+import { CoinInfo, StorageRegistry } from './types';
 import { Wallet } from './wallet';
+import CoinRegistry from './coin-info.json';
 
 let wallet: Wallet | undefined;
+
+export function queryCoinInfo(predicate: (info: CoinInfo) => boolean) {
+  return CoinRegistry.filter(predicate).map<Readonly<CoinInfo>>(Object.freeze);
+}
 
 export function setStorageRegistry(registry: StorageRegistry): void {
   if (wallet !== undefined) {
