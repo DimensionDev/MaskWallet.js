b21eba83d2337ab3daa13a0343741b2353d105a4
Septs <github@septs.pw>
2021-01-22 15:21:21 +0800

    Regular update: 2021-01-22T07:21:21Z

--- src/crypto-suite/aes.spec.ts
+++ src/crypto-suite/aes.spec.ts
@@ -5,10 +5,11 @@ import * as AES128CBC from './aes-128-cbc';
 import { fromHexString, toHexString } from './utils';
 
 describe('aes', () => {
+  const plaintext = 'TokenCoreX';
+  const key = Uint8Array.of(1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4);
+  const iv = Uint8Array.of(1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4);
+
   it('AES-128-CTR', async () => {
-    const plaintext = 'TokenCoreX';
-    const key = Uint8Array.of(1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4);
-    const iv = Uint8Array.of(1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4);
     const encrypted = 'e19e6c5923d33c587cf8';
     assert.equal(toHexString(await AES128CTR.encrypt(new TextEncoder().encode(plaintext), key, iv)), encrypted);
 
@@ -17,9 +18,6 @@ describe('aes', () => {
   });
 
   it('AES-128-CBC', async () => {
-    const plaintext = 'TokenCoreX';
-    const key = Uint8Array.of(1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4);
-    const iv = Uint8Array.of(1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4);
     const encrypted = '13d567987d7eced9c2154551bc37bc5f';
     assert.equal(toHexString(await AES128CBC.encrypt(new TextEncoder().encode(plaintext), key, iv)), encrypted);
 
