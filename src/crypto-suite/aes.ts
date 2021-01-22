f5c5394ed99c26002bd2766a89c0aaee9c3bb0ba
Septs <github@septs.pw>
2021-01-22 16:00:56 +0800

    Regular update: 2021-01-22T08:00:56Z

--- src/crypto-suite/aes.ts
+++ src/crypto-suite/aes.ts
@@ -19,9 +19,9 @@ function make(name: 'AES-CTR' | 'AES-CBC', action: 'encrypt' | 'decrypt') {
       throw new CryptoSuiteError(`Invalid IV length`);
     }
     // prettier-ignore
-    const params: AesCtrParams | AesCbcParams = name === 'AES-CTR'
-      ? { name: 'AES-CTR', length: 128, counter: iv }
-      : { name: 'AES-CBC', length: 128, iv };
+    const params = name === 'AES-CTR'
+      ? <AesCtrParams>{ name: 'AES-CTR', length: 128, counter: iv }
+      : <AesCbcParams>{ name: 'AES-CBC', length: 128, iv };
     const cipherkey = await crypto.subtle.importKey('raw', key, { name }, false, [action]);
     const ciphertext = await crypto.subtle[action](params, cipherkey, data);
     return new Uint8Array(ciphertext);
