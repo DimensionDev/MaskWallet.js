c080a8a36e1e57a2af69280422cab73c34e3751e
Septs <github@septs.pw>
2021-01-22 15:17:38 +0800

    Regular update: 2021-01-22T07:17:38Z

--- src/crypto-suite/aes-128-cbc.ts
+++ src/crypto-suite/aes-128-cbc.ts
@@ -8,7 +8,7 @@ export async function encrypt(data: Uint8Array, key: Uint8Array, iv: Uint8Array)
   if (key.length !== 16 || iv.length !== 16) {
     throw new CryptoSuiteError('Invalid key or iv length');
   }
-  const encrypted = await crypto.subtle.encrypt({ name, counter: iv, length }, await importKey(key), data);
+  const encrypted = await crypto.subtle.encrypt({ name, iv, length }, await importKey(key), data);
   return new Uint8Array(encrypted);
 }
 
@@ -16,7 +16,7 @@ export async function decrypt(data: Uint8Array, key: Uint8Array, iv: Uint8Array)
   if (key.length !== 16 || iv.length !== 16) {
     throw new CryptoSuiteError('Invalid key or iv length');
   }
-  const decrypted = await crypto.subtle.decrypt({ name, counter: iv, length }, await importKey(key), data);
+  const decrypted = await crypto.subtle.decrypt({ name, iv, length }, await importKey(key), data);
   return new Uint8Array(decrypted);
 }
 
--- src/crypto-suite/aes.spec.ts
+++ src/crypto-suite/aes.spec.ts
@@ -20,7 +20,10 @@ describe('aes', () => {
     const plaintext = 'TokenCoreX';
     const key = Uint8Array.of(1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4);
     const iv = Uint8Array.of(1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4);
-    const encrypted = await AES128CBC.encrypt(new TextEncoder().encode(plaintext), key, iv);
-    assert.equal(toHexString(encrypted), '13d567987d7eced9c2154551bc37bc5f');
+    const encrypted = '13d567987d7eced9c2154551bc37bc5f';
+    assert.equal(toHexString(await AES128CBC.encrypt(new TextEncoder().encode(plaintext), key, iv)), encrypted);
+
+    const decrypted = await AES128CBC.decrypt(fromHexString(encrypted), key, iv);
+    assert.equal(new TextDecoder().decode(decrypted), plaintext);
   });
 });
