172d04a927bb21eccedffa01d22957af0675625b
Septs <github@septs.pw>
2021-01-22 15:54:41 +0800

    Regular update: 2021-01-22T07:54:41Z

--- src/crypto-suite/aes.ts
+++ src/crypto-suite/aes.ts
@@ -20,7 +20,7 @@ function make(name: 'AES-CTR' | 'AES-CBC', action: 'encrypt' | 'decrypt') {
     }
     // prettier-ignore
     const params: AesCtrParams | AesCbcParams = name === 'AES-CTR'
-      ? { name: 'AES-CTR', length: 128, counter: iv,  }
+      ? { name: 'AES-CTR', length: 128, counter: iv }
       : { name: 'AES-CBC', length: 128, iv };
     const cipherkey = await crypto.subtle.importKey('raw', key, { name }, false, [action]);
     const ciphertext = await crypto.subtle[action](params, cipherkey, data);
