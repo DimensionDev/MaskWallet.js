959207694d4d9a69163314b32f9ecf65e138fdc4
Septs <github@septs.pw>
2021-01-22 12:39:39 +0800

    Regular update: 2021-01-22T04:39:39Z

--- src/crypto-suite/crypto-key/types.ts
+++ src/crypto-suite/crypto-key/types.ts
@@ -3,18 +3,18 @@ import { CurveType } from 'types';
 export type CryptoKey = PublicKey | PrivateKey;
 
 export interface PublicKey {
-  readonly curve: CurveType;
+  curve: CurveType;
+}
+
+export interface PrivateKey extends PublicKey {
+  curve: CurveType;
+  getPublicKey(): PublicKey;
+  sign(data: Uint8Array): string;
+  signRecoverable(data: Uint8Array): string;
 }
 
 export interface DeterministicPublicKey extends PublicKey {
   derive(path: string): this;
-}
-
-export interface PrivateKey extends PublicKey {
-  readonly curve: CurveType;
-  getPublicKey(): PublicKey;
-  sign(data: Uint8Array): string;
-  signRecoverable(data: Uint8Array): string;
 }
 
 export interface DeterministicPrivateKey extends PrivateKey, DeterministicPublicKey {
