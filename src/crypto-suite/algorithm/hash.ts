64e2f4b224b68dbdb35946ad7cfa8d88e68e9291
Septs <github@septs.pw>
2021-02-05 13:38:54 +0800

    Regular update: 2021-02-05T05:38:54Z
    Signed-off-by: Septs <github@septs.pw>

--- src/crypto-suite/algorithm/hash.ts
+++ src/crypto-suite/algorithm/hash.ts
@@ -1,9 +1,18 @@
 import crypto from '../driver';
+import { Keccak, SHA3 } from 'sha3';
 
 export async function makeKeyHash(data: Uint8Array): Promise<Uint8Array> {
-  return new Uint8Array(await sha256(await sha256(data)));
+  return new Uint8Array(await sha2$256(await sha2$256(data)));
 }
 
-export function sha256(data: ArrayBuffer) {
+export function sha2$256(data: ArrayBuffer) {
   return crypto.subtle.digest('SHA-256', data);
+}
+
+export function sha3$256(data: Buffer) {
+  return new SHA3(256).update(data).digest();
+}
+
+export function keccak$256(data: Buffer) {
+  return new Keccak(256).update(data).digest();
 }
