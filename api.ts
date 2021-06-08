b5b8e6107cbbbe0192f379dcf8e76faa290e05cf
jk234ert <handan0524@gmail.com>
2021-06-08 21:49:29 +0800

    feat: update wasm lib; add simple test.ts

--- api.ts
+++ api.ts
@@ -17,6 +17,8 @@ function send<I extends keyof Request, O extends keyof Response>(input: I, outpu
 
 export const Coin = api.Coin;
 export const StoredKeyType = api.StoredKeyType;
+export const StoredKeyImportType = api.StoredKeyImportType;
+export const StoredKeyExportType = api.StoredKeyExportType;
 
 export const loadStoredKey = send('param_load_stored_key', 'resp_load_stored_key');
 export const createStoredKey = send('param_create_stored_key', 'resp_create_stored_key');
@@ -27,12 +29,18 @@ export const getStoredKeyAccountCount = send('param_get_stored_key_account_count
 export const getStoredKeyAccount = send('param_get_stored_key_account', 'resp_get_stored_key_account');
 export const getStoredKeyAllAccounts = send('param_get_stored_key_all_accounts', 'resp_get_stored_key_all_accounts');
 export const getStoredKeyAccountsOfCoin = send('param_get_stored_key_accounts_of_coin', 'resp_remove_account_of_coin');
-export const getStoredKeyAccountsOfCoinAtPath = send('param_create_acccount_of_coin_at_path', 'resp_create_account_of_coin_at_path');
+export const createAccountOfCoinAtPath = send('param_create_acccount_of_coin_at_path', 'resp_create_account_of_coin_at_path');
 export const removeAccountsOfCoin = send('param_remove_accounts_of_coin', 'resp_remove_account_of_coin');
 export const removeAccountOfAddress = send('param_remove_account_of_address', 'resp_remove_account_of_address');
 export const exportPrivateKey = send('param_export_private_key', 'resp_export_private_key');
 export const exportPrivateKeyOfPath = send('param_export_private_key_of_path', 'resp_export_private_key');
 export const exportMnemonic = send('param_export_mnemonic', 'resp_export_mnemonic');
-export const exportKeyStoreJSON = send('param_export_key_store_json', 'resp_export_key_store_json');
+export const exportKeyStoreJSONOfAddress = send('param_export_key_store_json_of_address', 'resp_export_key_store_json');
 export const exportKeyStoreJSONOfPath = send('param_export_key_store_json_of_path', 'resp_export_key_store_json');
+export const exportUpdateKeyStorePassword = send('param_update_key_store_password', 'resp_update_key_store_password');
+export const exportUpdateKeyStoreAccountName = send('param_update_stored_key_account_name', 'resp_update_key_store_account_name');
 export const signTransaction = send('param_sign_transaction', 'resp_sign_transaction');
+export const getLibVersion = send('param_get_version', 'resp_get_version');
+export const validate = send('param_validation', 'resp_validate');
+export const getSupportImportTypes = send('param_get_stored_key_import_type', 'resp_get_stored_key_import_type');
+export const getSupportExportTypes = send('param_get_stored_key_export_type', 'resp_get_stored_key_export_type');
--- test.ts
+++ test.ts
@@ -1,60 +1,39 @@
-import { importMnemonic, Coin } from './api';
-
-function send<T extends keyof api.IMWRequest>(name: T, value: api.IMWRequest[T]) {
-  const message = new api.MWRequest({ [name]: value });
-  const payload = api.MWRequest.encode(message).finish();
-  const response = api.MWResponse.decode(request(payload));
-  // if (response.error) {
-  //   throw new Error(response.error.errorMsg);
-  // }
-  return response;
-}
+import { Coin, StoredKeyType } from './api';
+import * as api from './api';
 
 const password = 'example';
-const importMnemonic = new Param.ImportMnemonicStoredKeyParam({
-const response = importMnemonic({
-  name: 'example',
-  coin: Coin.Ethereum,
-  mnemonic: 'suffer artefact burst review network fantasy easy century mom unique pupil boy',
-  password: password,
-});
 
-<<<<<<< HEAD
-var base_derivation_path = "m/44'/60'/0'/0/";
-// 1. Start Import Mnemonic
-const startImport = new Date().getTime();
-const importResp = send('param_import_mnemonic', importMnemonic);
-let durationOfImport = new Date().getTime() - startImport;
-console.log(importResp);
-console.log(`Duration of import mnemonic: ${durationOfImport / 1000}s`);
+let storedKey1 = api.createStoredKey({ password: password });
+let storedKey2 = api.createStoredKey({ password: password });
 
-var stored_key_data = importResp.resp_import_mnemonic.StoredKey.data;
+console.log(`mnemonic of 1st storedKey: ${storedKey1.mnemonic}`);
+console.log(`mnemonic of 2nd storedKey: ${storedKey2.mnemonic}`);
+
+const testMnemonic = 'suffer artefact burst review network fantasy easy century mom unique pupil boy';
+let storedKeyImportFromMnemonic1 = api.importMnemonic({ mnemonic: testMnemonic, password: password });
+let storedKeyImportFromMnemonic2 = api.importMnemonic({ mnemonic: testMnemonic, password: password });
+console.log(`Hash of imported storedKey: ${storedKeyImportFromMnemonic1.StoredKey.hash}`);
+console.log(`Hash of imported storedKey: ${storedKeyImportFromMnemonic2.StoredKey.hash}`);
+
+const base_derivation_path = "m/44'/60'/0'/0/";
+
 const startDerive = new Date().getTime();
-const num_of_addresses = 100;
+const num_of_addresses = 1000;
+
 for (let i = 0; i < num_of_addresses; i++) {
-  let path = base_derivation_path + i;
-  // console.log('creating path: ' + path);
-  const addNewAddress = new Param.CreateStoredKeyNewAccountParam({
-    StoredKeyData: stored_key_data,
-    coin: Param.Coin.Ethereum,
+  const createAccountParam = {
+    StoredKeyData: storedKeyImportFromMnemonic1.StoredKey.data,
+    name: `account${i}`,
+    coin: Coin.Ethereum,
     derivationPath: base_derivation_path + i,
     password: password,
-  });
-
-  // console.log(addNewAddress);
-  const addNewAccountResp = send('param_create_acccount_of_coin_at_path', addNewAddress);
-  // console.log(addNewAccountResp);
-  stored_key_data = addNewAccountResp.resp_create_account_of_coin_at_path.storedKey.data;
-  // console.log('-------------------------------------');
+  };
+  let updatedStoredKey = api.createAccountOfCoinAtPath(createAccountParam);
+  storedKeyImportFromMnemonic1.StoredKey.data = updatedStoredKey.storedKey.data;
 }
+
 let durationOfDerive = new Date().getTime() - startDerive;
 console.log(`Duration of Derive: ${durationOfDerive / num_of_addresses / 1000}s`);
 
-const getAllAccounts = new Param.GetStoredKeyAllAccountParam({
-  data: stored_key_data,
-});
-const getAllAccountsResp = send('param_get_stored_key_all_accounts', getAllAccounts);
-// console.log(getAllAccountsResp.resp_get_stored_key_all_accounts.accounts);
-=======
-console.log(response);
->>>>>>> ecf38a070447d22c32e524f712d66b94dbc2a086
+const allAccounts = api.getStoredKeyAllAccounts({ storedKeyData: storedKeyImportFromMnemonic1.StoredKey.data });
+console.log(`Created ${allAccounts.accounts.length} accounts`);
